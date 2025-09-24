c5b051ba6d904887923efc85c6c1482bf6d4f926
ayaponzu2525 <ayaponzu200525@gmail.com>
2025-09-24 15:56:21 +0900

    資格取得＆名前変更

--- jekyll_docker.bat
+++ jekyll_docker.bat
@@ -1,1 +1,2 @@
-docker run -v %cd%:/srv/jekyll -it --rm -e TZ=Asia/Tokyo -p 4000:4000 jekyll/jekyll bash
+@echo off
+docker run --rm -v "%cd%":/srv/jekyll -p 4000:4000 -e TZ=Asia/Tokyo jekyll/jekyll bash -c "bundle install && bundle exec jekyll serve --watch --host 0.0.0.0"
